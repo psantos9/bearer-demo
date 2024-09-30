c7ff1328c30d147ee38a3110bf8876a23a1e007e
Paulo Santos <paulo@vontage.com.br>
2024-09-30 17:02:13 -0300

    first commit

---
+++ bundle/assets/main.js
@@ -0,0 +1,6 @@
+localStorage.setItem('email', 'iu')
+const a = 'https://www.google.com'
+const response = fetch('http://secure-api.com')
+eval('console.log("hi there")')
+const cmd = 'console.log("hi there")'
+eval(cmd)
